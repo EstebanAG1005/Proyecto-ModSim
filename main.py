ccba946ae51e2895333ceef2b84118b5a5c3148b
Juan Angel Carrera <angelitocarrera@gmail.com>
2023-11-10 11:09:38 -0600

    add: localisation asistentes

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from asistente import Asistente
 from seguridad import Seguridad  # Asegúrate de tener este módulo y clase
 import numpy as np
 from time import sleep
+import matplotlib.pyplot as plt
 
 # Duración total de la simulación en tiempo real (segundos)
-DURACION_SIMULACION_REAL = 180  # 3 minutos en este ejemplo
+DURACION_SIMULACION_REAL = 60  # 3 minutos en este ejemplo
 
-DURACION_CONCIERTO_MINUTOS = 180  # 3 horas
+DURACION_CONCIERTO_MINUTOS = 60  # 3 horas
 TIEMPO_POR_ITERACION_MINUTOS = 5
 ITERACIONES_TOTAL = DURACION_CONCIERTO_MINUTOS // TIEMPO_POR_ITERACION_MINUTOS
 
@@ -41,6 +42,11 @@ asistentes_en_baños = 0  # Nueva métrica
 asistentes_salidos = 0
 gasto_total = 0
 incidentes_detectados = 0  # Nueva métrica
+
+costo_en_escenario = []
+costo_en_comida = []
+costo_salidos = []
+costo_sin_lugar = []
 
 # Realizamos una simulación simple mostrando el movimiento
 # de los asistentes en cada paso
@@ -84,6 +90,39 @@ for iteracion in range(ITERACIONES_TOTAL):
         ]
     )
 
+    Locationasistentes_en_escenario = [
+        a
+        for a in asistentes
+        if any(
+            e["coords"][0] - e["dims"][0] / 2
+            <= a.x
+            <= e["coords"][0] + e["dims"][0] / 2
+            and e["coords"][1] - e["dims"][1] / 2
+            <= a.y
+            <= e["coords"][1] + e["dims"][1] / 2
+            for e in festival.escenarios
+        )
+    ]
+    Locationasistentes_en_comida = [
+        a
+        for a in asistentes
+        if np.hypot(
+            a.x - festival.zonas_comida[0]["coords"][0],
+            a.y - festival.zonas_comida[0]["coords"][1],
+        ) <= 5
+    ]
+    Locationasistentes_salidos = [
+        a
+        for a in asistentes
+        if a.estado == "salió"
+    ]
+    asistentes_faltantes = [
+        a for a in asistentes
+        if a not in Locationasistentes_en_escenario
+        and a not in Locationasistentes_en_comida
+        and a not in Locationasistentes_salidos
+    ]
+
     gasto_total = sum([a.gasto for a in asistentes])
 
     asistentes_en_baños = sum(
@@ -116,3 +155,26 @@ for iteracion in range(ITERACIONES_TOTAL):
     print(f"Gasto promedio por asistente: {gasto_total / len(asistentes)}")
     print(f"Incidentes detectados: {incidentes_detectados}")
     print(f"Asistentes en baños: {asistentes_en_baños}\n")
+
+# TODO: Graficar
+
+# iteraciones = list(range(1, ITERACIONES_TOTAL + 1))
+
+# plt.figure(figsize=(10, 5))  # Ajusta el tamaño de la gráfica según necesites
+# plt.plot(iteraciones, costo_en_escenario, label='Escenario',
+#          marker='o', color='red')
+# plt.plot(iteraciones, costo_en_comida, label='Comida',
+#          marker='1', color='green')
+# plt.plot(iteraciones, costo_salidos, label='Salidos',
+#          marker='2', color='blue')
+# plt.plot(iteraciones, costo_sin_lugar, label='Sin lugar',
+#          marker='3', color='yellow')
+
+# # Etiquetas y título
+# plt.xlabel('Iteración')
+# plt.ylabel('Costo')
+# plt.title('Costo por Ubicación en el Tiempo')
+# plt.legend()  # Para mostrar las leyendas de las líneas
+
+# # Muestra la gráfica
+# plt.show()
